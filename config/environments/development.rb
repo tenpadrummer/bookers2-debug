134930dfc79f3a30f31af301c83eb7b5c68e3ca2
tenpadrummer <yoshiro.kato@kate-lier.com>
2023-09-06 12:53:55 +0000

    Error resolved

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "57271c8a632346a485911cd7193b5080.vfs.cloud9.ap-northeast-1.amazonaws.com"
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
