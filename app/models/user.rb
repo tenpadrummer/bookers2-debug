a1c6d19c109a595fed1425d9ba46f3b6612a3942
tenpadrummer <yoshiro.kato@kate-lier.com>
2023-09-10 05:50:58 +0000

    add guest user function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  GUEST_USER_EMAIL = "guest@example.com"
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -67,5 +69,17 @@ class User < ApplicationRecord
     else
       User.all
     end
+  end
+
+  # ゲストユーザーのログイン処理
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+  
+  def guest_user?
+    email == GUEST_USER_EMAIL
   end
 end
